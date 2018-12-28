8cc11532724605bf9e362b45ac543214557bc1df
Ilya Kreymer <ikreymer@gmail.com>
2018-12-27 19:22:01 -0800

    make min-port and max-port writable!

--- ext/webrtc/gstwebrtcice.c
+++ ext/webrtc/gstwebrtcice.c
@@ -941,13 +941,13 @@ gst_webrtc_ice_class_init (GstWebRTCICEClass * klass)
       PROP_MIN_PORT,
       g_param_spec_uint ("min-port", "ICE candidate min port",
           "minimum port, if any, for local rtp port range",
-          0, 65535, 0, G_PARAM_READABLE | G_PARAM_STATIC_STRINGS));
+          0, 65535, 0, G_PARAM_READWRITE | G_PARAM_STATIC_STRINGS));
 
   g_object_class_install_property (gobject_class,
       PROP_MAX_PORT,
       g_param_spec_uint ("max-port", "ICE candidate max port",
           "maximum port, if any, for local rtp port range",
-          0, 65535, 0, G_PARAM_READABLE | G_PARAM_STATIC_STRINGS));
+          0, 65535, 0, G_PARAM_READWRITE | G_PARAM_STATIC_STRINGS));
 
   /**
    * GstWebRTCICE::on-ice-candidate:
